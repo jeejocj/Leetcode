021f58150646212fbc146ff67c6df21f640b9a6e
Jeejo Joshy <134997154+jeejocj@users.noreply.github.com>
2025-09-12 12:03:52 +0530

    Time: 0 ms (100%), Space: 55 MB (30.25%) - LeetHub

---
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} words
+ * @param {string} pref
+ * @return {number}
+ */
+var prefixCount = function(words, pref) {
+
+    let count = 0;
+
+    for (let word of words) {
+        // if (word.slice(0, pref.length) === pref) {  
+        //     count++;
+        // }
+        if(word.startsWith(pref)){
+            count++;
+        }
+    }
+
+    return count;
+};
