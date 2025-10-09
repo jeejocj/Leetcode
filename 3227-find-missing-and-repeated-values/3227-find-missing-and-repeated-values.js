98ded1b135430712c3405c2d30c1b6fba6fa6c3e
Jeejo Joshy <134997154+jeejocj@users.noreply.github.com>
2025-10-09 11:37:47 +0530

    Time: 94 ms (5.72%), Space: 66.2 MB (5.39%) - LeetHub

---
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} grid
+ * @return {number[]}
+ */
+var findMissingAndRepeatedValues = function(grid) {
+    let result = []
+    let flattened = grid.flat(Infinity)
+    console.log(flattened)
+    let map = {}
+
+    for(let num of flattened){
+         map[num]  = (map[num] || 0) +1
+    }
+
+    for(let key in map){
+        if(map[key] == 2){
+            result.push(+key)
+
+        }
+    }
+
+    flattened.sort()
+    for(let i=1;i<=flattened.length;i++){
+        if(!flattened.includes(i)) result.push(i)
+    }
+
+    return result
+   
+};
