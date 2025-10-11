df4a4d1bd3de49487181d0cec1e1e3b73dbb6828
Jeejo Joshy <134997154+jeejocj@users.noreply.github.com>
2025-10-11 10:18:08 +0530

    Time: 2 ms (98.84%), Space: 60.6 MB (67.6%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    let map = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 }
+    let result = 0
+    let prev = 0
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        let curr = map[s[i]]
+
+        if (curr < prev) {
+            result -= curr
+        } else {
+            result += curr
+        }
+
+        prev = curr
+
+    }
+
+    return result
+};
