e3f2ceb95396b2329afe1ba5186952f3b0a317d3
Jeejo Joshy <134997154+jeejocj@users.noreply.github.com>
2025-10-22 10:14:04 +0530

    Time: 3 ms (45.59%), Space: 58.1 MB (28.16%) - LeetHub

---
+++ 4068-sum-of-elements-with-frequency-divisible-by-k/4068-sum-of-elements-with-frequency-divisible-by-k.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var sumDivisibleByK = function(nums, k) {
+    let map = {}
+    for(let num of nums){
+       map[num] = (map[num] || 0 )+1
+    }
+let result = 0
+    for(let key in map){
+        if(map[key] % k==0){
+            
+            result += +key * map[key]
+        }
+    }
+
+    return result
+};
