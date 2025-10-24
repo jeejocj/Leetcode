1793bafbf38e21331c8ea506caa8073f205ec339
Jeejo Joshy <134997154+jeejocj@users.noreply.github.com>
2025-10-24 12:20:11 +0530

    Time: 6 ms (64.95%), Space: 57.7 MB (22.89%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+    let x = 0
+    let y= 0
+    for(let i=0;i<moves.length;i++){
+        if(moves[i] == 'U'){
+            x++
+        }else if(moves[i] == 'D'){
+            x--
+        }else if(moves[i] == 'R'){
+            y++
+        }else if(moves[i] == 'L'){
+            y--
+        }
+    }
+    if(x == 0 && y==0 ){
+        return true
+    }
+    return false
+};
