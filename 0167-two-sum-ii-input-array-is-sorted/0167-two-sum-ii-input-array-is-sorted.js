3ce5ab9065a474498ce056d7e984ece28a5a1ddb
Jeejo Joshy <134997154+jeejocj@users.noreply.github.com>
2025-12-02 12:06:20 +0530

    Time: 4 ms (18.91%), Space: 56 MB (21.43%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let map = new Map()
+
+    for(let i=0;i<numbers.length;i++){
+        let diff = target - numbers[i]
+        if(map.has(diff)) return [map.get(diff),i+1]
+        map.set(numbers[i],i+1)
+    }
+};
